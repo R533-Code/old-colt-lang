5320759ece73dcac5e7816b6f2b0c16bff71209a
R533-Code <raphaeldibnehmepc@gmail.com>
2022-10-02 16:27:32 +0300

    Added precompiled header

---
+++ src/util/colt_pch.h
@@ -0,0 +1,36 @@
+/** @file colt_pch.h
+* Colt precompiled header, contains includes used throughout multiple files.
+*/
+
+#ifndef HG_COLT_PCH
+#define HG_COLT_PCH
+
+#include <cstdlib>
+
+#include <utility>
+#include <fstream>
+
+#include <limits>
+
+#include <chrono>
+
+#include <mutex>
+#include <atomic>
+#include <thread>
+
+#include <algorithm>
+#include <charconv>
+
+//{fmt} includes
+#include <fmt/core.h>
+#include <fmt/color.h>
+#include <fmt/ranges.h>
+
+//Includes
+#include <util/colt_config.h>
+
+#include <util/colt_macro.h>
+#include <util/colt_print.h>
+#include <util/typedefs.h>
+
+#endif //!HG_COLT_PCH
