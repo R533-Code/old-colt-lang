0a86dcf6fe1022bca8612a93b0a909d2ed465021
R533-Code <raphaeldibnehmepc@gmail.com>
2022-10-03 21:49:04 +0300

    Replaced all std::string_view by StringView

--- src/parsing/colt_error_report.h
+++ src/parsing/colt_error_report.h
@@ -11,24 +11,23 @@
 namespace colt::lang
 {
 	template<typename... Args>
-	void generate_message(size_t line_nb, std::string_view line_strv, std::string_view lexeme, fmt::format_string<Args...> fmt, Args&&... args) noexcept;
+	void generate_message(size_t line_nb, StringView line_strv, StringView lexeme, fmt::format_string<Args...> fmt, Args&&... args) noexcept;
 
 	template<typename... Args>
-	void generate_warning(size_t line_nb, std::string_view line_strv, std::string_view lexeme, fmt::format_string<Args...> fmt, Args&&... args) noexcept;
+	void generate_warning(size_t line_nb, StringView line_strv, StringView lexeme, fmt::format_string<Args...> fmt, Args&&... args) noexcept;
 
 	template<typename... Args>
-	void generate_error(size_t line_nb, std::string_view line_strv, std::string_view lexeme, fmt::format_string<Args...> fmt, Args&&... args) noexcept;
+	void generate_error(size_t line_nb, StringView line_strv, StringView lexeme, fmt::format_string<Args...> fmt, Args&&... args) noexcept;
 	
 	template<typename ...Args>
-	void generate_message(size_t line_nb, std::string_view line_strv, std::string_view lexeme, fmt::format_string<Args...> fmt, Args&& ...args) noexcept
+	void generate_message(size_t line_nb, StringView line_strv, StringView lexeme, fmt::format_string<Args...> fmt, Args&& ...args) noexcept
 	{
-		assert_true(line_strv.find(lexeme) != std::string_view::npos, "lexeme should be part of line!");
 		if (!args::GlobalArguments->print_messages)
 			return;
 		io::print_message(fmt, std::forward<Args>(args)...);
 		
-		std::string_view begin_line = { line_strv.data(), lexeme.data() };
-		std::string_view end_line = { lexeme.data() + lexeme.size(), line_strv.data() + line_strv.size() };
+		StringView begin_line = { line_strv.get_data(), lexeme.get_data() };
+		StringView end_line = { lexeme.get_data() + lexeme.get_size(), line_strv.get_data() + line_strv.get_size() };
 		
 		size_t line_nb_size = fmt::formatted_size("{}", line_nb);
 		if (args::GlobalArguments->colored_output)
@@ -36,22 +35,21 @@ namespace colt::lang
 		else
 			io::print("{} | {}{}{}", line_nb, begin_line, lexeme, end_line);
 		
-		if (lexeme.empty() || lexeme.size() == 1)
-			io::print(" | {: <{}}^", "", begin_line.size());
+		if (lexeme.is_empty() || lexeme.get_size() == 1)
+			io::print(" | {: <{}}^", "", begin_line.get_size());
 		else
-			io::print("{: <{}} | {: <{}}{:~<{}}", "", line_nb_size, "", begin_line.size(), "", lexeme.size());
+			io::print("{: <{}} | {: <{}}{:~<{}}", "", line_nb_size, "", begin_line.get_size(), "", lexeme.get_size());
 	}
 
 	template<typename ...Args>
-	void generate_warning(size_t line_nb, std::string_view line_strv, std::string_view lexeme, fmt::format_string<Args...> fmt, Args&& ...args) noexcept
+	void generate_warning(size_t line_nb, StringView line_strv, StringView lexeme, fmt::format_string<Args...> fmt, Args&& ...args) noexcept
 	{
-		assert_true(line_strv.find(lexeme) != std::string_view::npos, "lexeme should be part of line!");
 		if (!args::GlobalArguments->print_warnings)
 			return;
 		io::print_warning(fmt, std::forward<Args>(args)...);
 
-		std::string_view begin_line = { line_strv.data(), lexeme.data() };
-		std::string_view end_line = { lexeme.data() + lexeme.size(), line_strv.data() + line_strv.size() };
+		StringView begin_line = { line_strv.get_data(), lexeme.get_data() };
+		StringView end_line = { lexeme.get_data() + lexeme.get_size(), line_strv.get_data() + line_strv.get_size() };
 		
 		size_t line_nb_size = fmt::formatted_size("{}", line_nb);
 		if (args::GlobalArguments->colored_output)
@@ -59,21 +57,20 @@ namespace colt::lang
 		else
 			io::print("{} | {}{}{}", line_nb, begin_line, lexeme, end_line);
 		
-		if (lexeme.empty() || lexeme.size() == 1)
-			io::print("{: <{}} | {: <{}}^", "", line_nb_size, "", begin_line.size());
+		if (lexeme.is_empty() || lexeme.get_size() == 1)
+			io::print("{: <{}} | {: <{}}^", "", line_nb_size, "", begin_line.get_size());
 		else
-			io::print("{: <{}} | {: <{}}{:~<{}}", "", line_nb_size, "", begin_line.size(), "", lexeme.size());
+			io::print("{: <{}} | {: <{}}{:~<{}}", "", line_nb_size, "", begin_line.get_size(), "", lexeme.get_size());
 	}
 	template<typename ...Args>
-	void generate_error(size_t line_nb, std::string_view line_strv, std::string_view lexeme, fmt::format_string<Args...> fmt, Args&& ...args) noexcept
+	void generate_error(size_t line_nb, StringView line_strv, StringView lexeme, fmt::format_string<Args...> fmt, Args&&... args) noexcept
 	{
-		assert_true(line_strv.find(lexeme) != std::string_view::npos, "lexeme should be part of line!");
 		if (!args::GlobalArguments->print_errors)
 			return;
 		io::print_error(fmt, std::forward<Args>(args)...);
 
-		std::string_view begin_line = { line_strv.data(), lexeme.data() };
-		std::string_view end_line = { lexeme.data() + lexeme.size(), line_strv.data() + line_strv.size() };
+		StringView begin_line = { line_strv.get_data(), lexeme.get_data() };
+		StringView end_line = { lexeme.get_data() + lexeme.get_size(), line_strv.get_data() + line_strv.get_size() };
 
 		size_t line_nb_size = fmt::formatted_size("{}", line_nb);
 		if (args::GlobalArguments->colored_output)
@@ -81,10 +78,10 @@ namespace colt::lang
 		else
 			io::print("{} | {}{}{}", line_nb, begin_line, lexeme, end_line);
 		
-		if (lexeme.empty() || lexeme.size() == 1)
-			io::print("{: <{}} | {: <{}}^", "", line_nb_size, "", begin_line.size());
+		if (lexeme.is_empty() || lexeme.get_size() == 1)
+			io::print("{: <{}} | {: <{}}^", "", line_nb_size, "", begin_line.get_size());
 		else
-			io::print("{: <{}} | {: <{}}{:~<{}}", "", line_nb_size, "", begin_line.size(), "", lexeme.size());
+			io::print("{: <{}} | {: <{}}{:~<{}}", "", line_nb_size, "", begin_line.get_size(), "", lexeme.get_size());
 	}
 }
 
