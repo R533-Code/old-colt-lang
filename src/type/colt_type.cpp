d60f9c9b6d7dc2f5e0a16917a26e5f94f33247e7
R533-Code <raphaeldibnehmepc@gmail.com>
2022-10-04 19:39:38 +0300

    Added empty file for the type system

---
+++ src/type/colt_type.cpp
@@ -0,0 +1,10 @@
+/** @file colt_type.cpp
+* Contains definition of functions declared in 'colt_type.h'.
+*/
+
+#include "colt_type.h"
+
+namespace colt::lang
+{
+	
+}
---
+++ src/type/colt_type.h
@@ -0,0 +1,12 @@
+#ifndef HG_COLT_TYPE
+#define HG_COLT_TYPE
+
+#include <util/colt_pch.h>
+
+/// @brief Contains helpers for types
+namespace colt::lang
+{
+
+};
+
+#endif //!HG_COLT_TYPE
