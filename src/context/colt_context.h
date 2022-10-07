8dc6250062811be4966597cc87b7ed608aeeff73
R533-Code <raphaeldibnehmepc@gmail.com>
2022-10-07 21:43:17 +0300

    Added add_expr

--- src/context/colt_context.h
+++ src/context/colt_context.h
@@ -3,11 +3,22 @@
 
 #include <util/colt_pch.h>
 
+#include <ast/colt_expr.h>
+#include <type/colt_type.h>
+
 namespace colt::lang
 {
   class COLTContext
   {
+    //StableSet<UniquePtr<Type>> type_set;
+    StableSet<UniquePtr<Expr>> expr_set;
 
+  public:
+    
+    PTR<Expr> add_expr(UniquePtr<Expr>&& expr) noexcept
+    {
+      return expr_set.insert(std::move(expr)).first->get_ptr();
+    }
   };
 }
 
