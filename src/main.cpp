a70644f88f8e77e3ef1a7fa97d5ca39ea6091db3
R533-Code <raphaeldibnehmepc@gmail.com>
2022-10-10 14:31:33 +0300

    Added test for lexer

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,17 @@ int main(int argc, const char** argv)
   //Populates GlobalArguments
   args::ParseArguments(argc, argv);
 
+  char buffer[2500];
+  while (!feof(stdin))
+  {
+    fgets(buffer, 2500, stdin);
+    lang::Lexer lexer = { buffer };
+
+    lang::Token tkn = lexer.get_next_token();
+    while (tkn != lang::TKN_EOF)
+      tkn = lexer.get_next_token();
+  }
+
   io::PrintMessage("Hello Colt!");
   io::PrintWarning("Hello Colt!");
   io::PrintError("Hello Colt!");
