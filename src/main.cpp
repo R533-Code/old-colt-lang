39acc7d8defe189be68d6c04445b19ac34e33635
R533-Code <raphaeldibnehmepc@gmail.com>
2022-10-15 17:39:32 +0300

    Added test for AST

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <util/colt_pch.h>
 #include <parsing/colt_lexer.h>
-#include <ast/colt_expr.h>
+#include <ast/colt_ast.h>
 
 using namespace colt;
 
@@ -9,20 +9,26 @@ int main(int argc, const char** argv)
   //Populates GlobalArguments
   args::ParseArguments(argc, argv);
 
+  io::PrintMessage("Hello Colt!");
+  io::PrintWarning("Hello Colt!");
+  io::PrintError("Hello Colt!");
+
   char buffer[2048];
   for (;;)
   {
     fgets(buffer, 2048, stdin);
     if (feof(stdin))
       break;
-
-    lang::Lexer lexer = { StringView{ buffer, WithNUL } };
-    lang::Token tkn = lexer.get_next_token();
-    while (tkn != lang::TKN_EOF)
-      tkn = lexer.get_next_token();
-  }
-
-  io::PrintMessage("Hello Colt!");
-  io::PrintWarning("Hello Colt!");
-  io::PrintError("Hello Colt!");
+    lang::COLTContext ctx;
+    if (auto AST = lang::CreateAST({ buffer, WithNUL }, ctx))
+    {
+      io::PrintMessage("Parsed successfully!", buffer);
+      if (is_a<lang::BinaryExpr*>(AST.get_value().expressions.get_back()))
+        io::PrintMessage("Parsed a binary expression!");
+    }
+    else
+    {
+      io::PrintError("Parsed unsuccessfully!", buffer);
+    }
+  }  
 }
