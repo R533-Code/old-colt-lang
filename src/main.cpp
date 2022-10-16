36b1920f7fcd874fea5ee50ad2bb53baf300fded
R533-Code <raphaeldibnehmepc@gmail.com>
2022-10-16 19:31:44 +0300

    Added message printing after compilation

--- src/main.cpp
+++ src/main.cpp
@@ -4,42 +4,53 @@
 using namespace colt;
 using namespace colt::lang;
 
-int main(int argc, const char** argv)
+void compile(StringView str) noexcept
 {
-  //Populates GlobalArguments
-  args::ParseArguments(argc, argv);
+  //Record beginning of compilation
+  auto begin_time = std::chrono::steady_clock::now();
 
-  io::PrintMessage("Hello Colt!");
-  io::PrintWarning("Hello Colt!");
-  io::PrintError("Hello Colt!");
+  //Compile
+  COLTContext ctx;
+  auto AST = CreateAST(str, ctx);
 
+  //Record end of compilation
+  io::PrintMessage("Finished compilation in {:.6}.",
+    std::chrono::duration_cast<std::chrono::duration<double>>(std::chrono::steady_clock::now() - begin_time));
+
+  if (AST.is_value())
+    io::PrintMessage("Compilation successful!");
+  else
+    io::PrintWarning("Compilation failed!");
+}
+
+void REPL() noexcept
+{
   char buffer[2048];
   for (;;)
   {
     fgets(buffer, 2048, stdin);
     if (feof(stdin))
-      break;
+      break;    
 
-    //Record beginning of compilation
-    auto begin_time = std::chrono::steady_clock::now();
+    compile({ buffer, WithNUL });
+  }
+}
 
-    //Compile
-    COLTContext ctx;
-    auto AST = CreateAST({ buffer, WithNUL }, ctx);
-    
-    //Record end of compilation
-    io::PrintMessage("Finished compilation in {:.6}.",
-      std::chrono::duration_cast<std::chrono::duration<double>>(std::chrono::steady_clock::now() - begin_time));
+int main(int argc, const char** argv)
+{
+  //Populates GlobalArguments
+  args::ParseArguments(argc, argv);
 
-    if (AST)
-    {
-      io::PrintMessage("Parsed successfully!", buffer);
-      if (is_a<BinaryExpr>(AST.get_value().expressions.get_back()))
-        io::PrintMessage("Parsed a binary expression!");
-    }
+  if (args::GlobalArguments.file_in)
+  {
+    auto str = String::getFileContent(args::GlobalArguments.file_in);
+    if (str.is_error())
+      io::PrintError("Error reading file at path '{}'.", args::GlobalArguments.file_in);
     else
-    {
-      io::PrintError("Parsed unsuccessfully!", buffer);
-    }
+      compile(str.get_value());
+  }
+  else
+  {
+    REPL();
   }
 }
