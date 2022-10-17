a4eb9f15cc74a7282bdde80d52c5e1216c6ab7a2
R533-Code <raphaeldibnehmepc@gmail.com>
2022-10-17 20:29:22 +0300

    Added declaration

--- src/context/colt_context.h
+++ src/context/colt_context.h
@@ -10,6 +10,8 @@ namespace colt
 {
   size_t hash(const lang::Expr& expr) noexcept;
   size_t hash(const lang::Type& expr) noexcept;
+  size_t hash(const UniquePtr<lang::Expr>& expr) noexcept;
+  size_t hash(const UniquePtr<lang::Type>& expr) noexcept;
 }
 
 namespace colt::lang
