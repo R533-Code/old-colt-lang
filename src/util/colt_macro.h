1dd08eeda622cc32aea32038cfd312f1c278b168
R533-Code <raphaeldibnehmepc@gmail.com>
2022-10-17 19:54:26 +0300

    Fixed COLT_FUNC

--- src/util/colt_macro.h
+++ src/util/colt_macro.h
@@ -24,7 +24,7 @@
 	#endif
 #endif
 
-#if defined(COLT_MSVC)
+#if !defined(COLT_MSVC)
 	#define COLT_FUNC __FUNCTION__
 #else
 	#define COLT_FUNC __func__
@@ -35,11 +35,11 @@
 	#define assert_true(cond, err) do { \
 			if (!(cond)) \
 			{ \
-				colt::io::PrintError("Assertion failure: '" #cond "' evaluated to false in function '"  COLT_FUNC "' on line {}:\nExplanation: " err "\nFile: " __FILE__, __LINE__); \
+				colt::io::PrintError("Assertion failure: '" #cond "' evaluated to false in function '' on line {}:\nExplanation: " err "\nFile: " __FILE__, COLT_FUNC, __LINE__); \
 				colt_intrinsic_dbreak(); \
 			} } while (0)
 	/// @brief Marks the current branch as unreachable, which aborts if hit on any configuration
-	#define colt_unreachable(err) do { colt::io::PrintError("Unreachable branch hit in function '"  COLT_FUNC "' on line {}.\nExplanation: {}\nFile: " __FILE__, __LINE__, err); colt_intrinsic_dbreak(); std::abort(); } while (0)
+	#define colt_unreachable(err) do { colt::io::PrintError("Unreachable branch hit in function '{}' on line {}.\nExplanation: {}\nFile: " __FILE__, COLT_FUNC, __LINE__, err); colt_intrinsic_dbreak(); std::abort(); } while (0)
 #else
 	/// @brief On Debug configuration, asserts that 'cond' is true
 	#define assert_true(cond, err)
