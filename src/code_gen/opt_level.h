b79d4f02b7ba32457afe8378d2b5a7c65612b8c5
R533-Code <raphaeldibnehmepc@gmail.com>
2022-12-11 17:45:13 +0200

    Fixed error with names of enums

--- src/code_gen/opt_level.h
+++ src/code_gen/opt_level.h
@@ -13,12 +13,12 @@ namespace colt::gen
 		/// @brief Disable nearly all optimization
 		O0 = 1,
 		/// @brief Optimize quickly without hindering debuggability.
-		01 = 2,
+		O1 = 2,
 		/// @brief Optimize for fast execution as much as possible without significantly
 		/// increasing compile times
-		02 = 3,
+		O2 = 3,
 		/// @brief Optimize for fast execution as much as possible.
-		03 = 4,
+		O3 = 4,
 		/// @brief Similar to O2 but tries to optimize for small code size instead of fast execution
 		Os = 5,
 		/// @brief A very specialized mode that will optimize for code size at any and all
