242ea6e09ca3a47bcea6fbb1d698ee53d6102d1e
R533-Code <raphaeldibnehmepc@gmail.com>
2022-12-20 16:11:23 +0200

    Fixed error printing assertion

--- src/util/colt_macro.h
+++ src/util/colt_macro.h
@@ -9,48 +9,48 @@
 #include <util/colt_print.h>
 
 #if defined(__has_builtin)
-	#if __has_builtin(__builtin_debugtrap)
-		/// @brief Intrinsic trap
-		#define colt_intrinsic_dbreak() __builtin_debugtrap()
-	#elif __has_builtin(__debugbreak)
-		/// @brief Intrinsic trap
-		#define colt_intrinsic_dbreak() __debugbreak()
-	#endif
+  #if __has_builtin(__builtin_debugtrap)
+    /// @brief Intrinsic trap
+    #define colt_intrinsic_dbreak() __builtin_debugtrap()
+  #elif __has_builtin(__debugbreak)
+    /// @brief Intrinsic trap
+    #define colt_intrinsic_dbreak() __debugbreak()
+  #endif
 #endif
 
 #ifndef colt_intrinsic_dbreak
-	#if defined(_MSC_VER) || defined(__INTEL_COMPILER)
-		/// @brief Intrinsic trap
-		#define colt_intrinsic_dbreak() __debugbreak()
-	#else
-		/// @brief Intrinsic trap
-		#define colt_intrinsic_dbreak() do { (void)std::fgetc(stdin); std::exit(1); } while (0)
-	#endif
+  #if defined(_MSC_VER) || defined(__INTEL_COMPILER)
+    /// @brief Intrinsic trap
+    #define colt_intrinsic_dbreak() __debugbreak()
+  #else
+    /// @brief Intrinsic trap
+    #define colt_intrinsic_dbreak() do { (void)std::fgetc(stdin); std::exit(1); } while (0)
+  #endif
 #endif
 
 #if !defined(COLT_MSVC)
-	/// @brief Current function name
-	#define COLT_FUNC __FUNCTION__
+  /// @brief Current function name
+  #define COLT_FUNC __FUNCTION__
 #else
-	/// @brief Current function name
-	#define COLT_FUNC __func__
+  /// @brief Current function name
+  #define COLT_FUNC __func__
 #endif
 
 #ifdef COLT_DEBUG
-	/// @brief On Debug configuration, asserts that 'cond' is true
-	#define assert_true(cond, err) do { \
-			if (!(cond)) \
-			{ \
-				colt::io::PrintError("Assertion failure: '" #cond "' evaluated to false in function '' on line {}:\nExplanation: " err "\nFile: " __FILE__, COLT_FUNC, __LINE__); \
-				colt_intrinsic_dbreak(); \
-			} } while (0)
-	/// @brief Marks the current branch as unreachable, which aborts if hit on any configuration
-	#define colt_unreachable(err) do { colt::io::PrintError("Unreachable branch hit in function '{}' on line {}.\nExplanation: {}\nFile: " __FILE__, COLT_FUNC, __LINE__, err); colt_intrinsic_dbreak(); std::abort(); } while (0)
+  /// @brief On Debug configuration, asserts that 'cond' is true
+  #define assert_true(cond, err) do { \
+      if (!(cond)) \
+      { \
+        colt::io::PrintError("Assertion failure: '" #cond "' evaluated to false in function '{}' on line {}:\nExplanation: " err "\nFile: " __FILE__, COLT_FUNC, __LINE__); \
+        colt_intrinsic_dbreak(); \
+      } } while (0)
+  /// @brief Marks the current branch as unreachable, which aborts if hit on any configuration
+  #define colt_unreachable(err) do { colt::io::PrintError("Unreachable branch hit in function '{}' on line {}.\nExplanation: {}\nFile: " __FILE__, COLT_FUNC, __LINE__, err); colt_intrinsic_dbreak(); std::abort(); } while (0)
 #else
-	/// @brief On Debug configuration, asserts that 'cond' is true
-	#define assert_true(cond, err)
-	/// @brief Marks the current branch as unreachable, which aborts if hit on any configuration
-	#define colt_unreachable(err) std::abort()
+  /// @brief On Debug configuration, asserts that 'cond' is true
+  #define assert_true(cond, err)
+  /// @brief Marks the current branch as unreachable, which aborts if hit on any configuration
+  #define colt_unreachable(err) std::abort()
 #endif
 
 /// @brief Internal concatenating macro
@@ -60,24 +60,24 @@
 
 namespace
 {
-	template<typename Fun>
-	struct ScopeGuard
-	{
-		Fun fn;
+  template<typename Fun>
+  struct ScopeGuard
+  {
+    Fun fn;
 
-		ScopeGuard(Fun&& fn) noexcept
-			: fn(std::forward<Fun>(fn)) {}
+    ScopeGuard(Fun&& fn) noexcept
+      : fn(std::forward<Fun>(fn)) {}
 
-		~ScopeGuard() noexcept { fn(); }
-	};
+    ~ScopeGuard() noexcept { fn(); }
+  };
 
-	enum class ScopeGuardOnExit {};
-	
-	template<typename Fun>
-	ScopeGuard<Fun> operator+(ScopeGuardOnExit, Fun&& fn) noexcept
-	{
-		return ScopeGuard<Fun>(std::forward<Fun>(fn));
-	}
+  enum class ScopeGuardOnExit {};
+  
+  template<typename Fun>
+  ScopeGuard<Fun> operator+(ScopeGuardOnExit, Fun&& fn) noexcept
+  {
+    return ScopeGuard<Fun>(std::forward<Fun>(fn));
+  }
 }
 
 /// @brief Register an action to be called at the end of the scope.
@@ -94,10 +94,10 @@ namespace
 
 #ifdef _WIN32
   /// @brief Makes a function available for JIT
-	#define COLT_EXPORT_FN __declspec(dllexport)
+  #define COLT_EXPORT_FN __declspec(dllexport)
 #else
-	/// @brief Makes a function available for JIT
-	#define COLT_EXPORT_FN
+  /// @brief Makes a function available for JIT
+  #define COLT_EXPORT_FN
 #endif
 
 /// @brief Expands to 'extern "C"'
