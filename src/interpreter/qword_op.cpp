ebd755faa95a0302cf212cf761a0fe089c1cc088
R533-Code <raphaeldibnehmepc@gmail.com>
2022-12-30 18:40:20 +0200

    Added new files

---
+++ src/interpreter/qword_op.cpp
@@ -0,0 +1,10 @@
+/** @file qword_op.cpp
+* Contains definition of functions declared in 'qword_op.h'.
+*/
+
+#include "qword_op.h"
+
+namespace colt::gen
+{
+
+}
---
+++ src/interpreter/qword_op.h
@@ -0,0 +1,13 @@
+/** @file qword_op.h
+* Contains function for QWORD operations.
+*/
+
+#ifndef COLT_HG_QWORD_OP
+#define COLT_HG_QWORD_OP
+
+namespace colt::gen
+{
+
+}
+
+#endif //!COLT_HG_QWORD_OP
