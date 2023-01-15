c43a3d76a3d39a574be12ab35b76635ccdf62b6e
R533-Code <raphaeldibnehmepc@gmail.com>
2023-01-15 14:21:21 +0200

    Upgraded highlighting code

--- src/io/colt_code_highlight.h
+++ src/io/colt_code_highlight.h
@@ -33,44 +33,45 @@ struct fmt::formatter<colt::io::HighlightCode>
 
     Lexer lex = { str.strv };
 
-    u64 old_offset = 1;
     Token tkn = lex.get_next_token();
-    u64 new_offset = lex.get_current_offset();
+    StringView lexeme = lex.get_current_lexeme();
+    u64 skipped_spaces = lex.get_skipped_spaces_count();
 
     while (tkn != TKN_EOF)
     {
       //Lookahead for function calls
       if (tkn == TKN_IDENTIFIER)
       {
-        StringView identifier = { str.strv.get_data() + old_offset - 1, str.strv.get_data() + new_offset - 1 };
-        old_offset = new_offset;
+        StringView identifier = lex.get_current_lexeme();
+        u64 skipped_spaces2 = lex.get_skipped_spaces_count();
 
         tkn = lex.get_next_token();
-        new_offset = lex.get_current_offset();
+        lexeme = lex.get_current_lexeme();
+        skipped_spaces = lex.get_skipped_spaces_count();
+        
         io::Color identifier_color;
         if (tkn == TKN_LEFT_PAREN)
           identifier_color = io::BrightYellowF;
         else
           identifier_color = io::BrightBlueF;
 
-        iter = fmt::format_to(iter, "{}{}", identifier_color, identifier);
-        iter = fmt::format_to(iter, "{}{}", io::ToColor(tkn),
-          StringView{ str.strv.get_data() + old_offset - 1, str.strv.get_data() + new_offset - 1 });
-
-        old_offset = new_offset;
+        iter = fmt::format_to(iter, "{: <{}}{}{}", "",
+          skipped_spaces2, identifier_color, identifier);
+        iter = fmt::format_to(iter, "{: <{}}{}{}",
+          "", skipped_spaces, io::ToColor(tkn), lexeme);
 
         tkn = lex.get_next_token();
-        new_offset = lex.get_current_offset();
+        lexeme = lex.get_current_lexeme();
+        skipped_spaces = lex.get_skipped_spaces_count();
 
         continue;
       }
-      iter = fmt::format_to(iter, "{}{}", io::ToColor(tkn),
-        StringView{ str.strv.get_data() + old_offset - 1,
-        str.strv.get_data() + new_offset - 1 * as<u64>(str.strv.get_size() != 1) });
-      old_offset = new_offset;
+      iter = fmt::format_to(iter, "{: <{}}{}{}",
+        "", skipped_spaces, io::ToColor(tkn), lexeme);
 
       tkn = lex.get_next_token();
-      new_offset = lex.get_current_offset();
+      lexeme = lex.get_current_lexeme();
+      skipped_spaces = lex.get_skipped_spaces_count();
     }    
     return fmt::format_to(iter, "{}{: <{}}", io::Reset, "", lex.get_skipped_spaces_count());
   }
